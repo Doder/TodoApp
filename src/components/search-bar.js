ed55ca5db66e7847f3c6230b93e93c6ed6e282e4
Aleksa <aleksadoderovic@gmail.com>
2017-08-29 09:00:12 +0200

    make search-bar controllable component

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = { term: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e){
+    e.preventDefault();
+    this.setState({term : e.target.value});
+    const {term} = this.state;
+    if(term.length > 1){
+      console.log(term);
+      //make search
+    }
+
+  }
+
   render(){
     return(
       <div className="centered" id="search-bar">
       <div className="row">
         <div className="col-md-12">
-            <input type="text" className="form-control" placeholder="Search" aria-label="Search"/>
+            <input type="text" className="form-control" placeholder="Search"
+            value={this.state.term} onChange={this.handleChange} aria-label="Search"/>
         </div>
         </div>
         </div>
